2d0666ae73556e6af205c8a9cba0e108bdfd529f
Gabriel Donato <gabrieldonato@brasilcard.net>
2022-06-28 20:44:54 -0300

    initial commit

---
+++ bin/lib/1_functions/1_functions.dart
@@ -0,0 +1,6 @@
+// Type Return  | Name Function | Parameters | Fuction body
+
+//Ex: Essa seria uma função de nivel superior, "uma função que está jogada no arquivo e não está
+// envolvida em uma classe".
+
+void main() {}
